822ea8f0bfd36592540ecdc5e163b9d92ac76822
s10lateef <s10lateef.wft@gmail.com>
2026-03-16 15:28:56 -0400

    initial python app with helm and argocd

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        'message': 'python app is working',
+        'routes': [
+            '/api/v1/details',
+            '/api/v1/healthz'
+        ]
+    })
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+        'hostname': socket.gethostname()
+    })
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
